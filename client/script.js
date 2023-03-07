37c0389c7d3357408e6907377468b768249d90a0
Roshan Raturi <roshanraturi804@gmail.com>
2023-03-07 12:45:07 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://roshanraturiserver.onrender.com/', {
+    const response = await fetch('https://myserver-5n29.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
